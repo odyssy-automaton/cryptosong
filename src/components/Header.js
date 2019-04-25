2a308f2bc3c1412be4a2593a210f9904951ef2e1
skuhlmann <samkuhlmann@gmail.com>
2019-04-25 10:25:33 -0600

    Adds suprise link component in header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
+
+import SurpriseLink from "./SurpriseLink";
+
 import "../styles/Header.scss";
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-      <div className="Header__Brand">
-        <a style={{ display: "block" }} href="/">
-          <img src="../../img/SongADayWorld__Logo--Standard.png"/>
-        </a>
-      </div>
-      <nav className="Header__Nav"></nav>
+        <div className="Header__Brand">
+          <a style={{ display: "block" }} href="/">
+            <img
+              src="../../img/SongADayWorld__Logo--Standard.png"
+              alt="Song A Day World"
+            />
+          </a>
+        </div>
+        <nav className="Header__Nav">
+          <SurpriseLink />
+        </nav>
       </div>
     );
   }
---
+++ src/components/SurpriseLink.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { get } from "../helpers/requests";
+
+class SurpriseLink extends Component {
+  state = {
+    songNumber: null
+  };
+
+  componentDidMount = () => {
+    console.log("mounting");
+    get("api/songs/count").then(count => {
+      const songNumber =
+        Math.floor(Math.random() * Math.floor(+count.data.number - 1)) + 1;
+
+      this.setState({ songNumber });
+    });
+  };
+
+  render() {
+    const { songNumber } = this.state;
+
+    return (
+      <div className="SurpriseLink">
+        <Link to={`song/${songNumber}`}>Surprise me</Link>
+      </div>
+    );
+  }
+}
+
+export default SurpriseLink;
