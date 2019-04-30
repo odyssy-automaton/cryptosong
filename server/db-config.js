600ee9c913c53888b82b9a561a5e38098a722dff
skuhlmann <samkuhlmann@gmail.com>
2019-04-30 15:00:05 -0600

    Losts of document-db trouble

--- server/db-config.js
+++ server/db-config.js
@@ -1,12 +1,23 @@
-require('dotenv').config()
-const Fawn = require('fawn');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const fs = require("fs");
+const certFileBuf = fs.readFileSync(__dirname + "/rds-combined-ca-bundle.pem");
+const Fawn = require("fawn");
+const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGODB_HOST, {
+const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
+
+const options = {
   promiseLibrary: global.Promise,
   useNewUrlParser: true
-});
+};
+
+// if (isInLambda) {
+//   options.ssl = true;
+//   options.sslCA = certFileBuf;
+// }
+
+mongoose.connect(process.env.MONGODB_HOST, options);
 
 Fawn.init(mongoose);
 
